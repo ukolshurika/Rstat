253d5a46e0732fb8ecddb2dce3f2ca168e345d25
kia84 <kia84@mail.msiu.ru>
2013-12-04 14:31:16 +0400

    Пусть 5.R сам решает, принимать ли гипотезы.

--- 5.R
+++ 5.R
@@ -4,10 +4,24 @@ b <- my_table[2:25]
 
 sink(stderr())
 
-t.test(a, b)
-t.test(a, b, alternative="less",var.equal=T)
-t.test(a, b, alternative="greater",var.equal=T)
+alpha = 0.1
 
-write("H0: alpha > p-value; отвергаем гипотезу", stdout())
-write("H1: alpha < p-value; принимаем гипотезу", stdout())
-write("H2: alpha > p-value; отвергаем гипотезу", stdout())
+res = t.test(a, b)
+res
+p1 = res[["p.value"]]
+
+res = t.test(a, b, alternative = "less", var.equal = T)
+res
+p2 = res[["p.value"]]
+
+res = t.test(a, b, alternative = "greater", var.equal = T)
+res
+p3 = res[["p.value"]]
+
+pretty_write = function(n, chr, a, p, expr){
+    write(paste(n, ": alpha [", a, "]", chr, "p-value [", p, "];", ifelse(expr, "принимаем", "отвергаем"), "гипотезу"), stdout())
+}
+
+pretty_write("H0", ">", alpha, p1, alpha > p1)
+pretty_write("H1", "<", alpha, p2, alpha < p2)
+pretty_write("H2", ">", alpha, p3, alpha > p3)
