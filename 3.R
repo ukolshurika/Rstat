ed7b5c41aa3d7adadfef178b40cd2dd4ffeebde3
Andrey Sitnikov <andrey.sitnikov0@ya.ru>
2013-12-11 15:52:58 +0400

    befor data input fix

--- 3.R
+++ 3.R
@@ -5,3 +5,20 @@ b <- my_table[2:25]
 sink(stderr())
 
 # some test and out here
+
+write("выборка a : ", stdout())
+a
+write("выборка b : ", stdout())
+b
+
+write(paste("выборочное среднее a =",sm_a <- mean(a)), stdout())
+write(paste("выборочное среднее b =",sm_b <- mean(b)), stdout())
+
+write(paste("выборочная дисперсия a =",sv_a <- var(a)), stdout())
+write(paste("выборочная дисперсия b =",sv_b <- var(b)), stdout())
+
+write(paste("среднее квадратичное отклонение a =",sd_a <- sd(a)), stdout())
+write(paste("среднее квадратичное отклонение b =",sd_b <- sd(b)), stdout())
+
+write(paste("выборочный коэффициент корреляции", cor(a,NULL)), stdout())
+
